75b69552e8b6c9ed9d757d863357b6272bfaeaa7
hlf-u <uludaghalef1808@gmail.com>
2021-01-31 22:24:55 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Kampı";
+            string kurs2 = "Programlamaya Giriş Kursu";
+            string kurs3 = "java";
+            string kurs4 = "python";
+            string[] kurslar = new string[] { "Yazılım Geliştirici Kampı", "Programlamaya Giriş Kursu" ,"java", "python"};
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+            Console.WriteLine("sayfa sonu");
+        }
+    }
+}
---
+++ Kampintro/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Kampintro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.35;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış Oku");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış Oku");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu");
+            }
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+            
+        }
+    }
+}
