5ff41984fcf3bb7933a95746dd7c58df742f748a
hlf-u <uludaghalef1808@gmail.com>
2021-02-02 23:41:12 +0300

    K

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Halef");
+            Console.WriteLine(isimler.Length);
+            isimler.Add("uludağ");
+            Console.WriteLine(isimler.Length);
+            foreach (var isim in isimler.Items   )
+            {
+                Console.WriteLine(isim);
+            }
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Makas";
+            product1.UnitPrice = 400;
+            product1.UnitsInStock = 12;
+
+            Product product2 = new Product { Id = 2, CategoryId = 3, ProductName = "Kalem", UnitsInStock = 8, UnitPrice = 56 };
+
+            //PascalCase    //camelCase
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+
+          
+        }
+    }
+}
